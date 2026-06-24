f7d2eb7294f0b7c424ff6ad1abbaca84b3e51f10
Henriquehouseinsti <casagranderique@usp.br>
2026-06-24 11:33:01 -0300

    Restructure project: rename actuator client, add ESP32-CAM client, update server config

---
+++ sistema_anticolisao_logistica/cliente_esp32_cam/src/main.cpp
@@ -0,0 +1,61 @@
+#include "Arduino.h"
+#include "WiFi.h"
+#include "esp_camera.h"
+#include "WebServer.h"
+#include "config.h"
+#include "camera_pins.h"
+
+WebServer server(CAMERA_STREAM_PORT);
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Camera init failed");
+    return;
+  }
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println();
+  Serial.print("Wi-Fi connected: ");
+  Serial.println(WiFi.localIP());
+
+  startCameraServer();
+}
+
+void loop() {
+  server.handleClient();
+}
